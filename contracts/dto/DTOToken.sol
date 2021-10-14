bc6af0ba1c30c5e3dc0a070b3217a51a0078b005
dotoracle <dotoracle.network@gmail.com>
2021-10-14 07:47:12 +0700

    DTOToken contract

---
+++ contracts/dto/DTOToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // for WETH
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // for WETH
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//token owner will be a time lock contract after farming started
+contract DTOToken is Context, Ownable, ERC20 {
+	uint256 public constant MAX_SUPPLY = 100000000e18;
+
+	constructor() public ERC20("DOTORACLE.NETWORK", "DTO") {
+		_mint(msg.sender, MAX_SUPPLY);
+    }
+}
