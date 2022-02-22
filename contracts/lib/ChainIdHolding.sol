a9aa6ed39db2f6d69b417ccb7f45af900a616a13
dotoracle <dotoracle.network@gmail.com>
2022-02-22 15:57:00 +0700

    redeploy bridge testnet

--- contracts/lib/ChainIdHolding.sol
+++ contracts/lib/ChainIdHolding.sol
@@ -1,9 +1,11 @@
 pragma solidity >=0.5.16;
+
 // import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 abstract contract ChainIdHolding {
     uint256 public chainId;
-    	function __ChainIdHolding_init() internal {
+
+    function __ChainIdHolding_init() internal {
         uint256 _cid;
         assembly {
             _cid := chainid()
@@ -11,6 +13,6 @@ abstract contract ChainIdHolding {
         chainId = _cid;
     }
     // constructor() internal {
-        
+
     // }
 }
---
+++ deploy/9_protocol_upgrade.js
@@ -0,0 +1,53 @@
+const {
+  chainNameById,
+  chainIdByName,
+  saveDeploymentData,
+  getContractAbi,
+  getTxGasCost,
+  log,
+  supportedChainIds,
+  approvers,
+} = require("../js-helpers/deploy");
+
+const _ = require("lodash");
+
+module.exports = async (hre) => {
+  const { ethers, upgrades, getNamedAccounts } = hre;
+  const { deployer, protocolOwner, trustedForwarder } =
+    await getNamedAccounts();
+  const network = await hre.network;
+  const deployData = {};
+
+  const chainId = chainIdByName(network.name);
+  const alchemyTimeout = chainId === 31337 ? 0 : chainId === 1 ? 5 : 3;
+
+  log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  log("DTO Multichain Bridge Protocol - Contract Deployment");
+  log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+  log("  Using Network: ", chainNameById(chainId));
+  log("  Using Accounts:");
+  log("  - Deployer:          ", deployer);
+  log("  - network id:          ", chainId);
+  log("  - Owner:             ", protocolOwner);
+  log("  - Trusted Forwarder: ", trustedForwarder);
+  log(" ");
+
+  log("  Deploying GenericBridge...");
+  const GenericBridgeAddress =
+    require(`../deployments/${chainId}/GenericBridge.json`).address;
+  const GenericBridge = await ethers.getContractFactory("GenericBridge");
+  await upgrades.upgradeProxy(GenericBridgeAddress, GenericBridge, [], {
+    unsafeAllow: ["delegatecall"],
+    unsafeAllowCustomTypes: true,
+    kind: "uups",
+    gasLimit: 1000000,
+  });
+
+  saveDeploymentData(chainId, deployData);
+  log('\n  Contract Deployment Data saved to "deployments" directory.');
+
+  log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+};
+
+module.exports.tags = ["protocolupgrade"];
