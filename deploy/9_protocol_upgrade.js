4149abd816a4159bd9e6fa2e84d66a83b47a9b45
dotoracle <dotoracle.network@gmail.com>
2023-06-14 03:05:49 +0700

    refactorings

--- deploy/9_protocol_upgrade.js
+++ deploy/9_protocol_upgrade.js
@@ -1,27 +1,19 @@
 const {
   chainNameById,
   chainIdByName,
-  saveDeploymentData,
-  getContractAbi,
-  getTxGasCost,
   log,
-  supportedChainIds,
-  approvers,
 } = require("../js-helpers/deploy");
 
-const _ = require("lodash");
 
 module.exports = async (hre) => {
   const { ethers, upgrades, getNamedAccounts } = hre;
   const { deployer, protocolOwner, trustedForwarder } =
     await getNamedAccounts();
   const network = await hre.network;
-  const deployData = {};
 
   const chainId = chainIdByName(network.name);
   if (chainId === 31337) return
 
-  const alchemyTimeout = chainId === 31337 ? 0 : chainId === 1 ? 5 : 3;
 
   log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
   log("DTO Multichain Bridge Protocol - Contract Deployment");
@@ -38,16 +30,10 @@ module.exports = async (hre) => {
   const GenericBridgeAddress =
     require(`../deployments/${chainId}/GenericBridge.json`).address;
   const GenericBridge = await ethers.getContractFactory("GenericBridge");
-  // let bridge = await upgrades.forceImport(GenericBridgeAddress, GenericBridge, {
-  //   kind: "uups",
-  //   gasLimit: 1000000,
-  // });
-
   await upgrades.upgradeProxy(GenericBridgeAddress, GenericBridge, {
     kind: "uups",
     gasLimit: 1000000,
   })
-  //saveDeploymentData(chainId, deployData);
   log('\n  Contract Deployment Data saved to "deployments" directory.');
 
   log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
