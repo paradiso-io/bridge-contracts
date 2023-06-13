3e0e7b3c77c7608e6526a859b40a30dcf11e4bd0
dotoracle <dotoracle.network@gmail.com>
2023-06-14 01:58:29 +0700

    add force import

---
+++ deploy/23_force_import_protocol.js
@@ -0,0 +1,41 @@
+const {
+  chainNameById,
+  chainIdByName,
+  log,
+} = require("../js-helpers/deploy");
+
+
+module.exports = async (hre) => {
+  const { ethers, upgrades, getNamedAccounts } = hre;
+  const { deployer, protocolOwner, trustedForwarder } =
+    await getNamedAccounts();
+  const network = await hre.network;
+
+  const chainId = chainIdByName(network.name);
+  if (chainId === 31337) return
+
+
+  log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  log("DTO Multichain Bridge Protocol - Contract Deployment");
+  log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+  log("  Using Network: ", chainNameById(chainId));
+  log("  Using Accounts:");
+  log("  - Deployer:          ", deployer);
+  log("  - network id:          ", chainId);
+  log("  - Owner:             ", protocolOwner);
+  log("  - Trusted Forwarder: ", trustedForwarder);
+  log(" ");
+  log("  Deploying GenericBridge...");
+  const GenericBridgeAddress =
+    require(`../deployments/${chainId}/GenericBridge.json`).address;
+  const GenericBridge = await ethers.getContractFactory("GenericBridge");
+  await upgrades.forceImport(GenericBridgeAddress, GenericBridge, {
+    kind: "uups",
+    gasLimit: 1000000,
+  });
+
+  log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+};
+
+module.exports.tags = ["forceimport"];
